9bc12b9abaf76f57c6e99b19dce985e047df28ca
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-04-03 07:10:32 +0700

    fix: ffmpeg arg

--- utils/metadataWriter.js
+++ utils/metadataWriter.js
@@ -2,19 +2,49 @@ const { unlinkSync, renameSync } = require("fs");
 const { spawn } = require("child_process");
 const path = require("path");
 
+const getParameter = (initialFileName, data) => {
+  if (data.album)
+    return [
+      "-i",
+      initialFileName,
+
+      "-metadata",
+      `title=${data.title}`,
+
+      "-metadata",
+      `album=${data.album}`,
+
+      "-metadata",
+      `artist=${data.artist}`,
+
+      "-c",
+      "copy",
+      "edited.mp3",
+    ];
+
+  return [
+    "-i",
+    initialFileName,
+
+    "-metadata",
+    `title=${data.title}`,
+
+    "-metadata",
+    `artist=${data.artist}`,
+
+    "-c",
+    "copy",
+    "edited.mp3",
+  ];
+};
+
 module.exports = (initialFileName, data, dir) =>
   new Promise((resolve) => {
+    const ffmpegArg = getParameter(initialFileName, data);
+
     const metadata = spawn(
       "ffmpeg",
-      [
-        "-i",
-        initialFileName,
-        "-metadata",
-        `title=${data.title}`,
-        "-c",
-        "copy",
-        "edited.mp3",
-      ],
+      ffmpegArg,
       { cwd: dir }
     );
 
